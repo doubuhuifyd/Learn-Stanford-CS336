f09bec5f5f5cc07e24d337a8f2121d3f16abd403
DOUBUHUI <182200908+doubuhuifyd@users.noreply.github.com>
2025-08-25 22:26:00 +0800

    add tokenizer

--- homework/assignment1_basics/config.py
+++ homework/assignment1_basics/config.py
@@ -1,1 +1,1 @@
-BPE_DATASET_PATH = r"D:\Learn-Stanford-CS336\homework\assignment1_basics\data\TinyStories-valid.txt"
+BPE_DATASET_PATH = r"D:\PyProject\Learn-Stanford-CS336\homework\assignment1_basics\data\TinyStories-valid.txt"
--- homework/assignment1_basics/cs336_basics/tokenizer.py
+++ homework/assignment1_basics/cs336_basics/tokenizer.py
@@ -2,13 +2,14 @@ __all__ = ["train_bpe_tokenizer"]
 
 import regex as re
 import os
-from typing import BinaryIO
-from collections import Counter, defaultdict
-from multiprocessing import Pool
-import time
+from typing import BinaryIO, Iterable, Iterator
+from collections import defaultdict
+from tqdm.contrib.concurrent import process_map
+from functools import partial
 import config
 
-PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+PAT =  re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+num_processes = 12
 
 
 def find_chunk_boundaries(
@@ -47,28 +48,27 @@ def find_chunk_boundaries(
     return sorted(set(chunk_boundaries))
 
 
-def pre_token(special_tokens: list, input_path: str) -> tuple[list, dict, dict]:
-    with open(input_path, "r", encoding="utf-8") as f:
-        # boundaries = find_chunk_boundaries(f, 5, "<|endoftext|>".encode("utf-8"))
-        # for start, end in zip(boundaries[:-1], boundaries[1:]):
-        #     f.seek(start)
-        #     chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        pattern = "|".join(map(re.escape, special_tokens))
-        chunks = re.split(pattern, f.read())
-        frequency_table = defaultdict(int)
-        chunk_list = []
-        for chunk in chunks:
-            ans = re.finditer(PAT, chunk)
-            for item in ans:
-                utf8_text = item.group().encode("utf-8")
-                frequency_table[utf8_text] += 1
-                chunk_list.append(utf8_text)
+def pre_token(file: str, special_tokens: list) -> tuple[defaultdict, defaultdict]:
+    if not special_tokens:
+        chunks = [file]
+    else:
+        special_tokens = sorted(special_tokens, key=len, reverse=True)
+        pattern = "|".join(re.escape(tok) for tok in special_tokens)
+        chunks = re.split(pattern, file)
+    word_table = defaultdict(int)
+    chunk_list = []
+    for chunk in chunks:
+        ans = PAT.finditer(chunk)
+        for item in ans:
+            utf8_text = item.group(0).encode("utf-8")
+            word_table[utf8_text] += 1
+            chunk_list.append(tuple(bytes([i]) for i in utf8_text))
     pair_table = defaultdict(int)
-    for key, value in frequency_table.items():
+    for key, value in word_table.items():
         for i in range(len(key) - 1):
-            pair = (key[i:i+1], key[i+1:i+2])
-            pair_table[pair] += 1
-    return chunk_list, frequency_table, pair_table
+            pair = (key[i:i + 1], key[i + 1:i + 2])
+            pair_table[pair] += value
+    return word_table, pair_table
 
 
 def train_bpe_tokenizer(
@@ -76,74 +76,104 @@ def train_bpe_tokenizer(
         vocab_size: int,
         special_tokens: list[str],
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    now = time.time()
     vocabulary_dict = {i: bytes([i]) for i in range(256)}
     vocabulary_num = 256
     for special_token in special_tokens:
         vocabulary_dict[vocabulary_num] = special_token.encode("utf-8")
         vocabulary_num += 1
 
-    # parameters_dict = {"chunks_index": chunks_index, "input_path": input_path}
-    # with Pool(processes=num_processes) as pool:
-    #     chunks, frequency_table, pair_table = pool.imap_unordered(pre_token, parameters_dict)
+    file = []
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, "<|endoftext|>".encode("utf-8"))
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            file.append(chunk)
 
-    chunks, frequency_table, pair_table = pre_token(special_tokens, input_path)
+    pre_token_func = partial(pre_token, special_tokens=special_tokens)
+    if len(file) < 5:
+        with open(input_path, "r", encoding="utf-8") as f:
+            file = f.read()
+        chunks, pair_table = pre_token_func(file)
+    else:
+        results = process_map(pre_token_func, file, max_workers=num_processes, chunksize=1)
+        chunks = []
+        word_table = defaultdict(int)
+        pair_table = defaultdict(int)
+        for word_tab, pair_tab in results:
+            for pair, count in word_tab.items():
+                word_table[pair] += count
+            for pair, count in pair_tab.items():
+                pair_table[pair] += count
+
     merges = []
     while vocabulary_num < vocab_size:
         merge_pairs = max(pair_table.items(), key=lambda x: (x[1], x[0]))[0]
         merges.append(merge_pairs)
         new_token_bytes = merge_pairs[0] + merge_pairs[1]
-        new_token_id = vocabulary_num
         vocabulary_dict[vocabulary_num] = new_token_bytes
         vocabulary_num += 1
         l, r = merge_pairs
-        del (pair_table[merge_pairs])
+        del pair_table[merge_pairs]
 
-        new_chunk = []
+        new_chunks = []
         for chunk in chunks:
-            # print(chunk)
             i = 0
-            merge_chunk = bytearray()
+            new_chunk = []
             while i < len(chunk):
-                if i < len(chunk) - 1 and chunk[i:i+1] == l and chunk[i+1:i+2] == r:
-                    merge_chunk.extend(new_token_bytes)
+                if i < len(chunk) - 1 and chunk[i] == l and chunk[i + 1] == r:
+                    new_chunk.append(new_token_bytes)
                     if i > 0:
-                        new_pair = (chunk[i-1:i], new_token_bytes)
+                        new_pair = (chunk[i - 1], new_token_bytes)
                         pair_table[new_pair] += 1
 
-                        left_pair = (chunk[i-1:i], l)
+                        left_pair = (chunk[i - 1], l)
                         pair_table[left_pair] -= 1
                         if pair_table[left_pair] <= 0:
                             del pair_table[left_pair]
 
                     if i < len(chunk) - 2:
-                        new_pair = (new_token_bytes, chunk[i+2:i+3])
+                        new_pair = (new_token_bytes, chunk[i + 2])
                         pair_table[new_pair] += 1
 
-                        right_pair = (r, chunk[i+2:i+3])
+                        right_pair = (r, chunk[i + 2])
                         pair_table[right_pair] -= 1
                         if pair_table[right_pair] <= 0:
                             del pair_table[right_pair]
 
                     i += 2
                 else:
-                    merge_chunk.extend(chunk[i:i+1])
+                    new_chunk.append(chunk[i])
                     i += 1
-
-            new_chunk.append(bytes(merge_chunk))
-        chunks = new_chunk
-
-    # print(time.time() - now, vocabulary_dict)
+            new_chunks.append(tuple(new_chunk))
+        chunks = new_chunks
     return vocabulary_dict, merges
 
 
-# if __name__ == "__main__":
-#     vocab_size = 1000
-#     num_processes = 12
-#     input_path = r"D:\Learn-Stanford-CS336\homework\assignment1_basics\data\TinyStories-valid.txt"
-#     special_tokens = ["<|endoftext|>"]
-#     chunks = ["low", "low", "low", "low", "low", "lower", "lower", "widest", "widest", "widest", "newest", "newest",
-#               "newest", "newest", "newest", "newest"]
-#     chunks = [tuple(chunk.encode("utf-8")) for chunk in chunks]
-#     pair_table = defaultdict(int)
-#     frequency_table = Counter(chunks)
+
+
+class Tokenizer():
+    def __init__(self, vocab: Dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
+        tokens = vocab.values()
+        vocab_size = len(tokens)
+        if special_tokens:
+            for special_token in special_tokens:
+                if special_token.encode("utf-8") not in tokens:
+                    vocab_size += 1
+                    vocab[vocab_size] = special_token.encode("utf-8")
+
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens
+
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
+        pass
+
+    def encode(self, text: str)-> list[int]:
+        pass
+
+    def encode_iterable(self, iterable: Iterable[str])->Iterator[int]:
+        pass
+
+    def decode(self, ids:list[int])-> str:
+        pass
